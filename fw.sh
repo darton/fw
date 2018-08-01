fc221cb8cda9ff5bffffa1fc34dbd76fe69c56ce
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-08-01 14:49:15 +0200

    update fw.sh

--- fw.sh
+++ fw.sh
@@ -42,7 +42,7 @@ sshurl=root@10.10.10.10
 dburl="mysql -s -u lmsd_reload lms -e \"select reload from hosts where id=4\""
 
 #PROXY IP ADDRESS
-proxy_ip=192.168.1.254
+proxy_ip=192.168.1.1
 
 #Ethernet interfaces
 #WAN=$(ip r|grep default |awk '{print $5}')
@@ -73,41 +73,11 @@ current_time=$(date '+%Y-%m-%d %H:%M:%S')
 
 source /opt/gateway/scripts/fwfunctions
 
-    lmsd_reload_new ()
-    {
-        #Sprawdza czy ustawiony jest status przeładowania dla demona lmsd na maszynie z LMS
-        lms_status=`ssh $sshurl "$dburl"| grep -v reload`
-    if [ $lms_status = 1 ]; then
-        echo "$current_time - Status przeładowania lmsd został ustawiony" >> $logdir/$logfile
-        ssh $sshurl '/usr/local/lmsd/bin/lmsd -q -h 127.0.0.1:3306 -H newgateway -u lmsd_reload -d lms'
-        echo "$current_time - Wykonałem reload lmsd na zdalnej maszynie" >> $logdir/$logfile
-        sleep 10
-        echo "$current_time - Pobieram konfigurację z LMS" >> $logdir/$logfile
-        get_config
-        echo "$current_time - Sprawdzam czy konieczny jest restart czy wystarczy reload" >> $logdir/$logfile
-#        nat_11_current_sha1sum=$(cat $oldconfdir/$nat_11_file |sort |sha1sum)
-#        nat_11_new_sha1sum=$(cat $confdir/$nat_11_file |sort |sha1sum)
-        nat_1n_current_sha1sum=$(cat $oldconfdir/$nat_1n_ip_file |sort |sha1sum)
-        nat_1n_new_sha1sum=$(cat $confdir/$nat_1n_ip_file |sort |sha1sum)
-
-#        if [ "$nat_11_current_sha1sum" != "$nat_11_new_sha1sum" ] || [ "$nat_1n_current_sha1sum" != "$nat_1n_new_sha1sum" ]
-        if  [ "$nat_1n_current_sha1sum" != "$nat_1n_new_sha1sum" ]; then
-            echo "$current_time - Wykonuję reload firewalla przez zmianę nat_1n_ip_file" >> $logdir/$logfile
-            #restart
-            newreload
-        else
-            echo "$current_time - Wykonuję reload firewalla przez zmianę innych plików" >> $logdir/$logfile
-            newreload
-        fi
-
-    else
-        echo "Status przeładowania lmsd nie został ustawiony, kończę program."
-    exit
-    fi
-    }
 
     maintenance-on ()
     {
+        echo "Firewall maintenance on"
+        echo "$current_time - Firewall maintenance on" >> $logdir/$logfile
         mpid=`cat /run/fw-sh/maintenance.pid`
         if [ $mpid = 1 ]; then
             echo ""
@@ -121,21 +91,24 @@ source /opt/gateway/scripts/fwfunctions
         echo ""
         echo "$current_time - Włączam tryb diagnostyczny" >> $logdir/$logfile
         echo ""
+        fw_cron stop
+        htb_cmd stop
         static_routing_down
         firewall_down
-        htb_cmd stop
         destroy_all_hashtables
-        fw_cron stop
         dhcpd_cmd stop
         ifup $MGMT
         ifdown $WAN
         ifdown $LAN
         echo 1 > /run/fw-sh/maintenance.pid
         fi
+        echo "$current_time - Firewall maintenance on OK" >> $logdir/$logfile
     }
 
     maintenance-off ()
     {
+        echo "Firewall maintenance off"
+        echo "$current_time - Firewall maintenance off" >> $logdir/$logfile
         mpid=`cat /run/fw-sh/maintenance.pid`
         if [ $mpid = 0 ]; then
             echo ""
@@ -156,21 +129,25 @@ source /opt/gateway/scripts/fwfunctions
         echo 0 > /run/fw-sh/maintenance.pid
         ifdown $MGMT
         fi
+        echo "$current_time - Firewall maintenance off OK" >> $logdir/$logfile
     }
 
     stop ()
     {
+        echo "Firewall Stop"
         echo "$current_time - Firewall Stop" >> $logdir/$logfile
         fw_cron stop
         htb_cmd stop
         static_routing_down
         firewall_down
         destroy_all_hashtables
+        echo "$current_time - Firewall Stop OK" >> $logdir/$logfile
     }
 
     start ()
     {
         #tuned-adm profile network-latency
+        echo "Firewall Start"
         stop
         echo "$current_time - Firewall Start" >> $logdir/$logfile
         static_routing_up
@@ -180,11 +157,7 @@ source /opt/gateway/scripts/fwfunctions
         htb_cmd start
         dhcpd_cmd start
         fw_cron start
-    }
-
-    lmsd ()
-    {
-        lmsd_reload_new
+        echo "$current_time - Firewall Start OK" >> $logdir/$logfile
     }
 
     newreload ()
@@ -196,7 +169,7 @@ source /opt/gateway/scripts/fwfunctions
         modify_nat1n_fw_rules
         htb_cmd restart
         dhcpd_cmd restart
-
+        echo "$current_time - Firewall newreload OK" >> $logdir/$logfile
     }
 
     restart ()
@@ -211,6 +184,7 @@ source /opt/gateway/scripts/fwfunctions
         firewall_up
         htb_cmd start
         dhcpd_cmd restart
+        echo "$current_time - Firewall restart OK" >> $logdir/$logfile
     }
 
     qos ()
@@ -218,6 +192,56 @@ source /opt/gateway/scripts/fwfunctions
         get_qos_config
         htb_cmd restart
     }
+
+    lmsd ()
+    {
+        lmsd_reload
+    }
+
+    lmsd_reload ()
+    {
+        #Sprawdza czy ustawiony jest status przeładowania dla demona lmsd na maszynie z LMS
+        lms_status=`ssh $sshurl "$dburl"| grep -v reload`
+    if [ $lms_status = 1 ]; then
+        echo "$current_time - Status przeładowania lmsd został ustawiony, wykonuje reload lmsd na zdalnej maszynie." >> $logdir/$logfile
+        ssh $sshurl '/usr/local/lmsd/bin/lmsd -q -h 127.0.0.1:3306 -H newgateway -u lmsd_reload -d lms'
+        echo "$current_time - Wykonałem reload lmsd na zdalnej maszynie, czekam 10s, aż lmsd stworzy nowe pliki konfiguracyjne" >> $logdir/$logfile
+        sleep 10
+        get_config
+        newreload
+    fi
+    }
+
+function lmsd_reload_old {
+#Sprawdza czy ustawiony jest status przeładowania dla demona lmsd na maszynie z LMS
+
+        lms_status=`ssh $sshurl "$dburl"| grep -v reload`
+    if [ $lms_status = 1 ]
+    then
+        echo "$current_time - Status przeładowania lmsd został ustawiony" >> /tmp/lms.status
+        echo "$current_time - Wykonuję reload lmsd na zdalnej maszynie" >> /tmp/lms.status
+        ssh $sshurl '/usr/local/lmsd/bin/lmsd -q -h 127.0.0.1:3306 -H newgateway -u lmsd_reload -d lms'
+        echo "$current_time - Czekam 10 sekund na wygenerowanie nowych plików konfiguracyjnych" >> /tmp/lms.status
+        sleep 10
+        echo "$current_time - Pobieram konfigurację z LMS" >> /tmp/lms.status
+        get_config
+        echo "$current_time - Sprawdzam czy konieczny jest restart czy wystarczy reload" >> /tmp/lms.status
+        nat_11_current_sha1sum=$(cat $oldconfdir/$nat_11_file |sort |sha1sum)
+        nat_11_new_sha1sum=$(cat $confdir/$nat_11_file |sort |sha1sum)
+        nat_1n_current_sha1sum=$(cat $oldconfdir/$nat_1n_ip_file |sort |sha1sum)
+        nat_1n_new_sha1sum=$(cat $confdir/$nat_1n_ip_file |sort |sha1sum)
+
+        if [ "$nat_11_current_sha1sum" != "$nat_11_new_sha1sum" ] || [ "$nat_1n_current_sha1sum" != "$nat_1n_new_sha1sum" ]; then
+            restart
+        else
+            newreload
+        fi
+    else
+        echo "Status przeładowania lmsd nie został ustawiony, kończę program."
+    exit
+    fi
+}
+
 
 #####Program główny####
 
