e99b26b6b6b7beaa2b38ac17c7a1512f0bf3c2d7
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2018-08-31 16:18:06 +0200

    update fw.sh

--- fw.sh
+++ fw.sh
@@ -30,7 +30,7 @@ public_ip_file=fw_public_ip
 routed_nets_file=fw_routed_ip
 blacklist_file=fw_blacklist
 lan_banned_dst_ports_file=fw_lan_banned_dst_ports
-shaper_file=fw_shaper_cfg
+shaper_file=fw_shaper
 dhcp_conf_file=dhcpd.conf
 
 #Source of config files
