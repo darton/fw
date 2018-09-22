55033f8076b3a3fc793e7fb7f397762fbf19c13b
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-09-22 23:53:44 +0200

    Update get_config.sh

--- get_config.sh
+++ get_config.sh
@@ -3,10 +3,11 @@
 . ./fw.conf
 confdir=./conf
 
-#Get config method from remote host
-#get_cmd="ssh $sshurl"
-#Get config method from localhost
-get_cmd="eval"
+#Exec command methods
+#On remote host
+#exec_cmd="ssh $sshurl"
+#on local host
+exec_cmd="eval"
 
 files_prefix=""
 
@@ -26,7 +27,7 @@ for host_status in {0..1}; do
     dbquery="SELECT INET_NTOA(ipaddr),INET_NTOA(ipaddr_pub) FROM nodegroupassignments nga JOIN nodes n ON nga.nodeid=n.id AND n.access=$host_status AND nga.nodegroupid=(SELECT id FROM nodegroups WHERE name='$forward_group_name');"
     dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
 
-    $get_cmd "$dburl"| while read ip ip_pub; do echo $status $ip; done >> $confdir/"$files_prefix"$public_ip_file
+    $exec_cmd "$dburl"| while read ip ip_pub; do echo $status $ip; done >> $confdir/"$files_prefix"$public_ip_file
 done
 
 
@@ -42,21 +43,21 @@ for host_status in {0..1}; do
     dbquery="SELECT INET_NTOA(ipaddr),INET_NTOA(ipaddr_pub) FROM nodegroupassignments nga JOIN nodes n ON nga.nodeid=n.id AND n.access=$host_status AND nga.nodegroupid=(SELECT id FROM nodegroups WHERE name='$nat11_group_name');"
     dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
 
-    $get_cmd "$dburl"| while read ip ip_pub; do echo $status $ip $ip_pub; done >> $confdir/"$files_prefix"$nat_11_file
+    $exec_cmd "$dburl"| while read ip ip_pub; do echo $status $ip $ip_pub; done >> $confdir/"$files_prefix"$nat_11_file
 done
 
 
 #IP address list of host belong to LMS groups nat_1n_*
 dbquery="SELECT id FROM nodegroups WHERE name like '$nat_1n_groups_name';"
 dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
-nat_1n_nodegroups_id=$($get_cmd "$dburl")
+nat_1n_nodegroups_id=$($exec_cmd "$dburl")
 
 cp /dev/null $confdir/"$files_prefix"$nat_1n_ip_file
 
 for item in $nat_1n_nodegroups_id; do
     dbquery="SELECT name FROM nodegroups WHERE id=$item;"
     dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
-    nat_1n_nodegroup_name=$($get_cmd "$dburl")
+    nat_1n_nodegroup_name=$($exec_cmd "$dburl")
     nat_1n_nodegroup_ip=$(echo $nat_1n_nodegroup_name | cut -c 9-)
 
     cp /dev/null $confdir/"$files_prefix"fw_$nat_1n_nodegroup_name
@@ -74,7 +75,7 @@ for item in $nat_1n_nodegroups_id; do
         dbquery="SELECT INET_NTOA(n.ipaddr) FROM nodegroupassignments nga JOIN nodes n ON nga.nodeid=n.id AND n.access=$host_status AND nga.nodegroupid=$item;"
         dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
 
-#       $get_cmd "$dburl"| while read ip; do echo $status $ip $nat_1n_nodegroup_ip; done > $confdir/"$files_prefix"fw_$nat_1n_nodegroup_name
-        $get_cmd "$dburl"| while read ip; do echo $status $ip; done >> $confdir/"$files_prefix"fw_$nat_1n_nodegroup_name
+#       $exec_cmd "$dburl"| while read ip; do echo $status $ip $nat_1n_nodegroup_ip; done > $confdir/"$files_prefix"fw_$nat_1n_nodegroup_name
+        $exec_cmd "$dburl"| while read ip; do echo $status $ip; done >> $confdir/"$files_prefix"fw_$nat_1n_nodegroup_name
     done
 done
