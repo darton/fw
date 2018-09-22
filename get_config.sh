f157c775d2b75fc053e6047ec4fb408141381991
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-09-22 23:39:50 +0200

    Update get_config.sh

--- get_config.sh
+++ get_config.sh
@@ -10,11 +10,12 @@ get_cmd="eval"
 
 files_prefix=""
 
-#IP address list of host belong to LMS group public_ip
 forward_group_name="public_ip"
+nat11_group_name="nat_1-1"
+nat_1n_groups_name="nat_1-n_%"
 
+#IP address list of host belong to LMS group public_ip
 cp /dev/null $confdir/"$files_prefix"$public_ip_file
-
 for host_status in {0..1}; do
     if [ "$host_status" = "1" ]; then
         status="grantedhost"
@@ -26,14 +27,11 @@ for host_status in {0..1}; do
     dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
 
     $get_cmd "$dburl"| while read ip ip_pub; do echo $status $ip; done >> $confdir/"$files_prefix"$public_ip_file
-done;
+done
 
 
 #IP address list of host belong to LMS group nat_1-1
-nat11_group_name="nat_1-1"
-
 cp /dev/null $confdir/"$files_prefix"$nat_11_file
-
 for host_status in {0..1}; do
     if [ "$host_status" = "1" ]; then
         status="grantedhost"
@@ -45,13 +43,10 @@ for host_status in {0..1}; do
     dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
 
     $get_cmd "$dburl"| while read ip ip_pub; do echo $status $ip $ip_pub; done >> $confdir/"$files_prefix"$nat_11_file
-
-done;
+done
 
 
 #IP address list of host belong to LMS groups nat_1n_*
-nat_1n_groups_name="nat_1-n_%"
-
 dbquery="SELECT id FROM nodegroups WHERE name like '$nat_1n_groups_name';"
 dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
 nat_1n_nodegroups_id=$($get_cmd "$dburl")
@@ -59,7 +54,6 @@ nat_1n_nodegroups_id=$($get_cmd "$dburl")
 cp /dev/null $confdir/"$files_prefix"$nat_1n_ip_file
 
 for item in $nat_1n_nodegroups_id; do
-
     dbquery="SELECT name FROM nodegroups WHERE id=$item;"
     dburl="mysql -s -u $lms_dbuser $lms_db -e \"$dbquery\""
     nat_1n_nodegroup_name=$($get_cmd "$dburl")
@@ -82,7 +76,5 @@ for item in $nat_1n_nodegroups_id; do
 
 #       $get_cmd "$dburl"| while read ip; do echo $status $ip $nat_1n_nodegroup_ip; done > $confdir/"$files_prefix"fw_$nat_1n_nodegroup_name
         $get_cmd "$dburl"| while read ip; do echo $status $ip; done >> $confdir/"$files_prefix"fw_$nat_1n_nodegroup_name
-    done;
-
+    done
 done
-
