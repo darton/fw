661469bff8e659513e347dc1a6d0045d1eceee39
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-09-24 11:01:59 +0200

    Update get_config.sh

--- get_config.sh
+++ get_config.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#  (C) Copyright Dariusz Kowalczyk
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License Version 2 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#get config fot fw.sh from LMS (lms.org.pl) data base.
+
 . ./fw.conf
 confdir=./conf
 
