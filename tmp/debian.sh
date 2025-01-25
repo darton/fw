b0a42c36ec3c773dc0cce354d70f4d8b4f6c4419
Dariusz Kowalczyk <darton@users.noreply.github.com>
2025-01-25 16:32:05 +0100

    Update debian.sh

--- tmp/debian.sh
+++ tmp/debian.sh
@@ -3,3 +3,6 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install ulogd2 iptables ipset isc-dhcp-server -y
+#po instalacji fw.sh konieczne kroki
+sudo cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.org
+sudo ln -s /opt/gateway/conf/dhcpd.conf /etc/dhcp/dhcpd.conf
