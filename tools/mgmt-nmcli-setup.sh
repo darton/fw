40e2faf0fc2fa8bd26e740d7d7c66c4a3a1707a4
Dariusz Kowalczyk <darton@users.noreply.github.com>
2025-02-14 14:27:17 +0100

    Update mgmt-nmcli-setup.sh

--- tools/mgmt-nmcli-setup.sh
+++ tools/mgmt-nmcli-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Setup for MGMT interface
+nmcli connection add type ethernet ifname eno1 con-name MGMT ip4 10.0.253.251/24 gw4 10.0.253.254
 nmcli con mod MGMT ipv4.route-table 200
 nmcli con mod MGMT ipv4.routing-rules "priority 5 from 10.0.253.251 table 200"
 nmcli con mod MGMT ipv4.route-metric 600
